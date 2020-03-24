5d70ac62a9d284df0a79edb8cb7c76ade0e51bbb
SaktiDw <sakti596@gmail.com>
2020-03-25 01:24:52 +0800

    Fixing navigasi button

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,5 +1,5 @@
 $('document').ready(function () {
-  $("button.menu-trigger").click(function(){
+  $("nav button").click(function(){
     $("ul.menu, button.menu-trigger span, body").toggleClass("menu-open");
   });
 })
