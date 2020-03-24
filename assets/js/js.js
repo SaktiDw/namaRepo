dd33c35f8824923e3353239054a2a248cbc84100
SaktiDw <sakti596@gmail.com>
2020-03-25 02:09:50 +0800

    fixing booking toggle

--- assets/js/js.js
+++ assets/js/js.js
@@ -1,5 +1,10 @@
 $('document').ready(function () {
+  // open close menu
   $("nav button").click(function(){
     $("ul.menu, button.menu-trigger span, body").toggleClass("menu-open");
+  });
+  // open close booking
+  $("section#konsul button, button.close").click(function(){
+    $(".booking").toggleClass("show");
   });
 })
